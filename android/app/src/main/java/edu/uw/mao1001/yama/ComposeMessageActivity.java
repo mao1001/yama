3f99fd3af451dfe5937af1d50bf1ff4a95310569
Garmega <garudaomega@gmail.com>
2016-05-01 21:07:53 -0700

    added back navigation

--- android/app/src/main/java/edu/uw/mao1001/yama/ComposeMessageActivity.java
+++ android/app/src/main/java/edu/uw/mao1001/yama/ComposeMessageActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Nick on 5/1/2016.
@@ -20,15 +21,35 @@ public class ComposeMessageActivity extends AppCompatActivity {
 
         setTitle(R.string.title_compose_message);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button button = (Button)findViewById(R.id.button_send_message);
+        if (button != null) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    sendMessage();
+                }
+            });
+        }
+
+
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
+                returnHome();
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void sendMessage() {
+        returnHome();
+    }
+
+    private void returnHome() {
+        NavUtils.navigateUpFromSameTask(this);
     }
 }
